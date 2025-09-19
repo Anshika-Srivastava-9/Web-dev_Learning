27d376a262aa7a1209b0354e03c956956d3b13a0
Anshika-Srivastava-9 <srianshika89@gmail.com>
2025-09-19 23:41:31 +0530

    JavaScript files

---
+++ JavaScript tutorial/54-Introduction to JavaScript/script.js
@@ -0,0 +1,15 @@
+alert("Hello and congrats you reached till here .Keep going😉✌️ All the Best👍")
+console .log("Code is running....")
+console .log("Code is looking nice..")
+console .log("Here are your first console codes...")
+console .log("keep Coding....")
+ 
+var a=prompt("enter your number")
+var isTrue=confirm("Are you sure you want to submit this number")
+if(isTrue){
+    console.log("number submitted")
+}
+else{
+    console.log("number not submitted")}
+
+    console.log("your number is"+a)
